3e25e5a5df12b5c252d99313a7cc209655a0e177
Chao Gao <86901408+ChaoGaoUCR@users.noreply.github.com>
2022-02-13 17:45:09 -0800

    Delete HelloPass-LLVM directory

--- HelloPass-LLVM/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd test
-clang-10 -S -fno-discard-value-names -emit-llvm test1.c -o test1.ll
-clang-10 -S -fno-discard-value-names -emit-llvm test2.c -o test2.ll
-clang-10 -S -fno-discard-value-names -emit-llvm test3.c -o test3.ll
-clang-10 -S -fno-discard-value-names -emit-llvm test4.c -o test4.ll
-clang-10 -S -fno-discard-value-names -emit-llvm test5.c -o test5.ll
